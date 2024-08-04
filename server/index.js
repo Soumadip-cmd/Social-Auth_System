788addc55ca3a08854c330c8845d0686402347b9
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-08-05 00:23:12 +0530

    updated change

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const path = require("path");
 const connectToMongo = require("./Db");
 const cors = require("cors");
 const passport = require("passport");
-//check
 const passportJs = require('./Passport');
 const passportAuth = require('./Routes/Passport.auth');
 
